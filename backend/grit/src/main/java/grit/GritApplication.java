748d4c5382d94d84531e12eb99f904dbc02b1785
Junyoung Yang <y@jun0.dev>
2025-11-18 17:52:09 +0900

    SCRUM-6 feat: SpringBoot 프로젝트 초기 생성

---
+++ backend/grit/src/main/java/grit/GritApplication.java
@@ -0,0 +1,13 @@
+package grit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GritApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GritApplication.class, args);
+    }
+
+}
