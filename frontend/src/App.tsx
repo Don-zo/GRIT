a7e076b34fd7d853c6d2656d6baa077fa3754a9d
Yun-yeong Kim <143103801+surferwithus@users.noreply.github.com>
2025-11-18 21:19:11 +0900

    feat: playlist card 구현 (#5)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
+import PlaylistCard from "./components/PlaylistCard/PlaylistCard";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="min-h-screen flex bg-tomato items-center justify-center">
+      <PlaylistCard />
+    </div>
+  );
 }
 
 export default App;
