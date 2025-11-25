e8d10c4354db8a3ba8594f53658125dcc1f43c44
yunyeong-kim <dbsdud3272@naver.com>
2025-11-25 20:51:09 +0900

    SCRUM-20 feat: 사용자 수 별 cam 분할 화면 구현

--- frontend/src/pages/Room/index.tsx
+++ frontend/src/pages/Room/index.tsx
@@ -3,9 +3,21 @@ import BottomBar from "@/pages/Room/components/BottomBar/BottomBar";
 import TopBar from "./components/TopBar/TopBar";
 import { NameBadge } from "./components/Cam/NameBadge";
 import Avatar from "./components/Cam/Avatar";
+import CamLayout from "./components/Cam/CamLayout";
 
 const RoomPage = () => {
   const [muted, setMuted] = useState(false);
+
+  const participants = [
+    { id: "p1", video: <div className="bg-red-500 w-full h-full" /> },
+    { id: "p2", video: <div className="bg-blue-500 w-full h-full" /> },
+    { id: "p3", video: <div className="bg-green-500 w-full h-full" /> },
+    { id: "p4", video: <div className="bg-yellow-500 w-full h-full" /> },
+    { id: "p5", video: <div className="bg-purple-500 w-full h-full" /> },
+    { id: "p6", video: <div className="bg-purple-500 w-full h-full" /> },
+    // { id: "p7", video: <div className="bg-purple-500 w-full h-full" /> },
+    // { id: "p8", video: <div className="bg-purple-500 w-full h-full" /> },
+  ];
 
   return (
     <div className="flex flex-col w-full h-screen bg-gray-darkest">
@@ -13,13 +25,14 @@ const RoomPage = () => {
       <TopBar />
       
       {/* 본 내용 */}
-      <div className="flex items-center justify-center flex-1">
-        <NameBadge
+      <div className="flex flex-1 mx-20 items-center justify-center">
+        {/* <NameBadge
           name="이유민이유민"
           isMuted={muted}
           onToggleMute={() => setMuted(!muted)}
         />
-        <Avatar />
+        <Avatar /> */}
+        <CamLayout participants={participants} />
       </div>
 
       {/* 하단바 */}
