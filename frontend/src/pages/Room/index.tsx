3eb7605bdc5e258804de7f20dc0eb1bbbb64c6b0
yunyeong-kim <dbsdud3272@naver.com>
2025-11-25 21:13:35 +0900

    feat: cam 화면에 nameBadge와 Avatar 적용

--- frontend/src/pages/Room/index.tsx
+++ frontend/src/pages/Room/index.tsx
@@ -1,22 +1,17 @@
-import React, { useState } from "react";
 import BottomBar from "@/pages/Room/components/BottomBar/BottomBar";
 import TopBar from "./components/TopBar/TopBar";
-import { NameBadge } from "./components/Cam/NameBadge";
-import Avatar from "./components/Cam/Avatar";
 import CamLayout from "./components/Cam/CamLayout";
 
 const RoomPage = () => {
-  const [muted, setMuted] = useState(false);
-
   const participants = [
-    { id: "p1", video: <div className="bg-red-500 w-full h-full" /> },
-    { id: "p2", video: <div className="bg-blue-500 w-full h-full" /> },
-    { id: "p3", video: <div className="bg-green-500 w-full h-full" /> },
-    { id: "p4", video: <div className="bg-yellow-500 w-full h-full" /> },
-    { id: "p5", video: <div className="bg-purple-500 w-full h-full" /> },
-    { id: "p6", video: <div className="bg-purple-500 w-full h-full" /> },
-    // { id: "p7", video: <div className="bg-purple-500 w-full h-full" /> },
-    // { id: "p8", video: <div className="bg-purple-500 w-full h-full" /> },
+    { id: "p1", name: "김윤영", isMuted: false },
+    { id: "p2", name: "양준영", isMuted: true },
+    { id: "p3", name: "이유민", isMuted: false },
+    { id: "p4", name: "이차현", isMuted: false },
+    { id: "p5", name: "김윤영김윤영", isMuted: true },
+    { id: "p6", name: "양준영양준영", isMuted: false },
+    // { id: "p7", name: "이유민이유민", isMuted: true },
+    // { id: "p8", name: "이차현이차현", isMuted: false },
   ];
 
   return (
@@ -26,12 +21,6 @@ const RoomPage = () => {
       
       {/* 본 내용 */}
       <div className="flex flex-1 mx-20 items-center justify-center">
-        {/* <NameBadge
-          name="이유민이유민"
-          isMuted={muted}
-          onToggleMute={() => setMuted(!muted)}
-        />
-        <Avatar /> */}
         <CamLayout participants={participants} />
       </div>
 
