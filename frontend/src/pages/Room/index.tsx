2bb5ed9b37332b3123ac7b0cc85a68062345d7fa
chahyunlee <chahyun023@gmail.com>
2025-11-25 22:07:37 +0900

    style: camlayout 여백 설정

--- frontend/src/pages/Room/index.tsx
+++ frontend/src/pages/Room/index.tsx
@@ -9,7 +9,7 @@ const RoomPage = () => {
     { id: "p3", name: "이유민", isMuted: false },
     { id: "p4", name: "이차현", isMuted: false },
     { id: "p5", name: "김윤영김윤영", isMuted: true },
-    { id: "p6", name: "양준영양준영", isMuted: false },
+    //{ id: "p6", name: "양준영양준영", isMuted: false },
     // { id: "p7", name: "이유민이유민", isMuted: true },
     // { id: "p8", name: "이차현이차현", isMuted: false },
   ];
@@ -18,7 +18,7 @@ const RoomPage = () => {
     <div className="flex flex-col w-full h-screen bg-gray-darkest">
       {/* 상단바 */}
       <TopBar />
-      
+
       {/* 본 내용 */}
       <div className="flex flex-1 mx-20 items-center justify-center">
         <CamLayout participants={participants} />
