900fed4ce4fd5ed13ee47990e38abfcccc629682
ymin431 <ymin431@naver.com>
2025-11-30 22:03:09 +0900

    style: 뽀모도로 인원별 레이아웃

--- frontend/src/pages/Room/index.tsx
+++ frontend/src/pages/Room/index.tsx
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import BottomBar from "@/pages/Room/components/BottomBar/BottomBar";
-import TopBar from "./components/TopBar/TopBar";
-import { NameBadge } from "./components/Cam/NameBadge";
-import Avatar from "./components/Cam/Avatar";
-import Pomodoro from "./components/Cam/Pomodoro";
-import CamLayout from "./components/Cam/CamLayout";
+import TopBar from "@/pages/Room/components/TopBar/TopBar";
+import Pomodoro from "@/pages/Room/components/Cam/Pomodoro";
+import CamLayout from "@/pages/Room/components/Cam/CamLayout";
 
 type PomodoroConfig = {
   studyMinutes: number;
@@ -20,23 +18,43 @@ const RoomPage = () => {
     { id: "p3", name: "이유민", isMuted: false },
     { id: "p4", name: "이차현", isMuted: false },
     { id: "p5", name: "김윤영김윤영", isMuted: true },
-    //{ id: "p6", name: "양준영양준영", isMuted: false },
-    // { id: "p7", name: "이유민이유민", isMuted: true },
-    // { id: "p8", name: "이차현이차현", isMuted: false },
+    { id: "p6", name: "양준영양준영", isMuted: false },
+    { id: "p7", name: "이유민이유민", isMuted: true },
+    { id: "p8", name: "이차현이차현", isMuted: false },
   ];
   const [muted, setMuted] = useState(false);
-  const [pomodoroConfig, setPomodoroConfig] = useState<PomodoroConfig | null>(
-    null
-  );
+  const [pomodoroConfig, setPomodoroConfig] = useState<PomodoroConfig>({
+    studyMinutes: 45,
+    breakMinutes: 15,
+    repeat: 1,
+    enabled: false,
+  });
 
+  const handlePomodoroFinish = () => {
+    setPomodoroConfig((prev) => ({
+      ...prev,
+      enabled: false,
+    }));
+  };
+
+  const pomodoroNode = (
+    <Pomodoro
+      studyMinutes={pomodoroConfig.studyMinutes}
+      breakMinutes={pomodoroConfig.breakMinutes}
+      repeat={pomodoroConfig.repeat}
+      autoStart={pomodoroConfig.enabled}
+      onFinish={handlePomodoroFinish}
+    />
+  );
+  
   return (
     <div className="flex flex-col w-full h-screen bg-gray-darkest">
       {/* 상단바 */}
       <TopBar />
 
       {/* 본 내용 */}
-      <div className="flex flex-1 mx-20 items-center justify-center">
-        <CamLayout participants={participants} />
+      <div className="flex items-center justify-center flex-1 mx-20">
+        <CamLayout participants={participants} pomodoro={pomodoroNode} />
       </div>
 
       {/* 하단바 */}
