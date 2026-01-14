beca41a151b09a586f0a350c60151e80d9376c8c
Yun-Yeong Kim <143103801+surferwithus@users.noreply.github.com>
2026-01-14 18:10:02 +0900

    feat: 친구 추가, 삭제 및 목록 조회 구현

---
+++ backend/grit/src/main/java/grit/friend/FriendController.java
@@ -0,0 +1,41 @@
+package grit.friend;
+
+import grit.friend.dto.FriendResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/friends")
+public class FriendController {
+    private final FriendService friendService;
+
+    // 친구 추가
+    @PostMapping("/{nickname}")
+    public ResponseEntity<FriendResponseDTO> addFriend(@PathVariable String nickname) {
+        Long currentUserId = 1L; // 임시 ID
+
+        FriendResponseDTO addedFriend = friendService.addFriend(currentUserId, nickname);
+
+        return ResponseEntity.ok(addedFriend);
+    }
+
+    // 친구 제거
+    @DeleteMapping("/{nickname}")
+    public ResponseEntity<FriendResponseDTO> removeFriend(@PathVariable String nickname) {
+        Long currentUserId = 1L; // 임시 ID
+
+        FriendResponseDTO removedFriend = friendService.removeFriend(currentUserId, nickname);
+
+        return ResponseEntity.ok(removedFriend);
+    }
+
+    // 친구 목록 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<List<FriendResponseDTO>> findFriends(@PathVariable Long id) {
+        return ResponseEntity.ok(friendService.getFriendList(id));
+    }
+}
--- backend/grit/src/main/java/grit/user/UserRepository.java
+++ backend/grit/src/main/java/grit/user/UserRepository.java
@@ -6,11 +6,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     // 회원 찾기 기능
-    /*
     Optional<User> findByEmail(String email);
     Optional<User> findByNickname(String nickname);
-    Optional<User> findByEmailAndNickname(String email, String nickname);
-    */
 
     // 존재 여부 확인
     boolean existsByNickname(String nickname);
