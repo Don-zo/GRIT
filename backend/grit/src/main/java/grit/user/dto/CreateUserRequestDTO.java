83ccc04207bb5af6efff814e6a7c5e4824a380a7
Yun-yeong Kim <143103801+surfwithus@users.noreply.github.com>
2026-01-19 00:41:30 +0900

    docs: 사용자 및 친구 API swagger 명세 작성 (#47)
    Co-authored-by: Yun-Yeong Kim <143103801+surferwithus@users.noreply.github.com>

--- backend/grit/src/main/java/grit/user/dto/CreateUserRequestDTO.java
+++ backend/grit/src/main/java/grit/user/dto/CreateUserRequestDTO.java
@@ -1,14 +1,20 @@
 package grit.user.dto;
 
 import grit.user.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CreateUserRequestDTO {
+    @Schema(description = "닉네임", example = "그릿유저")
     private String nickname;
+
+    @Schema(description = "비밀번호", example = "grit1234")
     private String password;
+
+    @Schema(description = "이메일", example = "grit1234@a.com")
     private String email;
 
     // DTO를 엔티티로 변환
