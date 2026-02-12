c92158fe1c5dc7e9a1d86a574a12ed716d2f29a5
chahyunlee <chahyun023@gmail.com>
2026-02-12 11:46:03 +0900

    feat: 랜딩페이지 라우팅 설정 및 폴더 구조 배치

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "@/pages/Home";
+import LandingPage from "@/pages/Landing";
 import RoomPage from "@/pages/Room";
 import { PATHS } from "@/routes/path";
 
@@ -7,6 +8,10 @@ export const router = createBrowserRouter([
   {
     path: PATHS.HOME,
     element: <HomePage />,
+  },
+  {
+    path: PATHS.LANDING,
+    element: <LandingPage />,
   },
   {
     path: PATHS.ROOM,
