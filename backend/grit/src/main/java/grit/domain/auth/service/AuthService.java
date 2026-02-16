27c69f90b1420b965614ce5b4ee6908951a62943
Junyoung Yang <y@jun0.dev>
2026-02-17 04:25:52 +0900

    feat: Google OAuth2 기반 가입/로그인/토큰발급 절차 구현 완료

---
+++ backend/grit/src/main/java/grit/domain/auth/service/AuthService.java
@@ -0,0 +1,33 @@
+package grit.domain.auth.service;
+
+import grit.domain.auth.infrastructure.oauth.GoogleOAuthClient;
+import grit.domain.member.constant.SocialProvider;
+import grit.domain.member.entity.Member;
+import grit.domain.member.service.MemberService;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AuthService {
+
+    private final GoogleOAuthClient googleOAuthClient;
+    private final MemberService memberService;
+
+    @Transactional
+    public Member authenticateWithGoogle(String code) {
+        OAuth2User oauth2User = googleOAuthClient.getOAuth2User(code);
+
+        String providerId = oauth2User.getAttribute("id");
+        String email = oauth2User.getAttribute("email");
+
+        Optional<Member> member = memberService.getBySocialAccount(SocialProvider.GOOGLE, providerId);
+        return member.orElseGet(
+                () -> memberService.createMember(email, SocialProvider.GOOGLE, providerId));
+
+    }
+}
