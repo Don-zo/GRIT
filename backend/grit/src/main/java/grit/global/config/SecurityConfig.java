80dc41a66b43d70f67b0f8d485ab86874eec6191
Junyoung Yang <y@jun0.dev>
2026-02-16 16:04:52 +0900

    feat: SecurityConfig애 session STATELESS 설정

--- backend/grit/src/main/java/grit/global/config/SecurityConfig.java
+++ backend/grit/src/main/java/grit/global/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,6 +24,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // 테스트를 위해 CSRF 보안 잠시 끄기
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll() // 일단 모든 요청을 허용 (나중에 수정)
                 );
