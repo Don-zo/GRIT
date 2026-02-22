7616dbd23bed9ddaa595545c73dc7d27db728e8d
Junyoung Yang <y@jun0.dev>
2026-02-23 03:43:15 +0900

    refactor: MemberController에 AuthenticationPrincipal 적용 완료 (Google OAuth 구현 완료로 인한)

--- backend/grit/src/main/java/grit/domain/member/controller/MemberController.java
+++ backend/grit/src/main/java/grit/domain/member/controller/MemberController.java
@@ -1,18 +1,18 @@
 package grit.domain.member.controller;
 
+import grit.domain.auth.infrastructure.jwt.MemberPrincipal;
+import grit.domain.member.entity.Member;
 import grit.domain.member.service.MemberService;
-import grit.domain.member.dto.MemberCreateRequestDto;
 import grit.domain.member.dto.MemberUpdateRequestDto;
 import grit.domain.member.dto.MemberResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,9 +22,10 @@ import java.util.List;
 @RequestMapping("/api/members")
 @RequiredArgsConstructor
 public class MemberController {
+
     private final MemberService memberService;
 
-    @Operation(summary = "회원 가입", description = "신규 사용자를 등록합니다.")
+    /*@Operation(summary = "회원 가입", description = "신규 사용자를 등록합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "회원가입 성공"),
             @ApiResponse(responseCode = "409", description = "이미 존재하는 이메일 또는 닉네임", content = @Content),
@@ -34,7 +35,7 @@ public class MemberController {
     public ResponseEntity<MemberResponseDto> create(@RequestBody MemberCreateRequestDto request) {
         //Member member = memberService.join(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(null);
-    }
+    }*/
 
     @Operation(summary = "전체 회원 조회", description = "전체 사용자를 조회합니다.")
     @ApiResponse(responseCode = "200", description = "조회 성공")
@@ -49,9 +50,10 @@ public class MemberController {
             @ApiResponse(responseCode = "404", description = "존재하지 않는 사용자 ID", content = @Content)
     })
     @GetMapping("/{id}")
-    public ResponseEntity<MemberResponseDto> findOne(@Parameter(description = "사용자 고유 ID (PK)", example = "1") @PathVariable Long id) {
-        MemberResponseDto response = memberService.findOne(id);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<MemberResponseDto> findOne(
+            @AuthenticationPrincipal MemberPrincipal memberPrincipal) {
+        Member member = memberService.findOne(memberPrincipal.id());
+        return ResponseEntity.ok(new MemberResponseDto(member));
     }
 
     @Operation(summary = "정보 수정", description = "사용자의 정보(닉네임, 비밀번호, 한 줄 소개)를 수정합니다. 3개 중 하나만 응답 바디에 적어도 정상적으로 수정됩니다.")
@@ -62,11 +64,13 @@ public class MemberController {
             @ApiResponse(responseCode = "409", description = "이미 사용 중인 닉네임", content = @Content)
     })
     @PutMapping("/{id}")
-    public ResponseEntity<MemberResponseDto> update(@Parameter(description = "사용자 고유 ID (PK)", example = "1") @PathVariable Long id, @RequestBody MemberUpdateRequestDto updateParam) {
-        memberService.update(id, updateParam);
+    public ResponseEntity<MemberResponseDto> update(
+            @AuthenticationPrincipal MemberPrincipal memberPrincipal,
+            @RequestBody MemberUpdateRequestDto updateParam) {
+        memberService.update(memberPrincipal.id(), updateParam);
 
-        MemberResponseDto updateUser = memberService.findOne(id);
-        return ResponseEntity.ok(updateUser);
+        Member updatedMember = memberService.findOne(memberPrincipal.id());
+        return ResponseEntity.ok(new MemberResponseDto(updatedMember));
     }
 
     @Operation(summary = "회원 탈퇴", description = "사용자를 삭제(탈퇴)합니다.")
@@ -75,8 +79,9 @@ public class MemberController {
             @ApiResponse(responseCode = "404", description = "존재하지 않는 사용자")
     })
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@Parameter(description = "사용자 고유 ID (PK)", example = "1") @PathVariable Long id) {
-        memberService.delete(id);
+    public ResponseEntity<Void> delete(
+            @AuthenticationPrincipal MemberPrincipal memberPrincipal) {
+        memberService.delete(memberPrincipal.id());
         return ResponseEntity.noContent().build();
     }
 }
