65176d4cda894ca6931c4aa2be4da6e862347392
Junyoung Yang <y@jun0.dev>
2026-03-01 19:25:14 +0900

    feat: 멤버 프로필 최초 설정용 API 및 멤버 닉네임 중복 여부 확인 API 추가

--- backend/grit/src/main/java/grit/domain/member/service/MemberService.java
+++ backend/grit/src/main/java/grit/domain/member/service/MemberService.java
@@ -2,22 +2,19 @@ package grit.domain.member.service;
 
 import grit.domain.member.constant.Role;
 import grit.domain.member.constant.SocialProvider;
-import grit.domain.member.dto.MemberUpdateRequestDto;
-import grit.domain.member.dto.MemberResponseDto;
 import grit.domain.member.entity.Member;
 import grit.domain.member.repository.MemberRepository;
+import grit.global.exception.EntityNotFoundException;
 import java.util.Optional;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class MemberService {
+
     private final MemberRepository memberRepository;
 
     // 회원 가입
@@ -34,11 +31,12 @@ public class MemberService {
         return memberRepository.save(member);
     }
 
-    // 닉네임 중복 체크
-    private void validNickname(String nickname) {
-        if (memberRepository.existsByNickname(nickname)) {
-            throw new IllegalStateException("이미 존재하는 닉네임입니다.");
+    public boolean isNicknameTaken(Member currentMember, String nickname) {
+        if (nickname.equals(currentMember.getNickname())) {
+            return true;
         }
+
+        return memberRepository.existsByNickname(nickname);
     }
 
     // 이메일 중복 체크
@@ -50,58 +48,36 @@ public class MemberService {
         }
     }
 
-    // 회원 전체 조회
-    public List<MemberResponseDto> findAll() {
-        return memberRepository.findAll().stream()
-                .map(MemberResponseDto::new)
-                .toList();
+    // 단일 회원 조회
+    public Member findMemberById(Long id) {
+        return memberRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 회원입니다."));
     }
 
-    // 단일 회원 조회
-    public Member findOne(Long id) {
-        return memberRepository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException(id + " 회원을 찾을 수 없습니다."));
+    @Transactional
+    public void initializeProfile(
+            Member member, String nickname, String introduction, String image) {
+
+        if (isNicknameTaken(member, nickname)) {
+            throw new IllegalStateException("이미 사용 중인 닉네임입니다.");
+        }
+        member.initializeProfile(nickname, introduction, image);
     }
+
 
     // 정보 수정
     @Transactional
-    public void update(Long id, MemberUpdateRequestDto updateParam) {
-        Member member = findMemberById(id);
+    public void updateProfile(
+            Member member, String nickname, String introduction, String image) {
 
-        updateNickname(member, updateParam.getNickname());
-        updateIntroduction(member, updateParam.getIntroduction());
-    }
-
-    // 사용자 조회 로직
-    private Member findMemberById(Long id) {
-        return memberRepository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 회원입니다."));
-    }
-
-    // 닉네임 수정
-    private void updateNickname(Member member, String newNickname) {
-        if (newNickname == null) return;
-
-        if (newNickname.equals(member.getNickname()))
-            throw new IllegalStateException("변경하려는 닉네임이 기존 닉네임과 동일합니다.");
-
-        if (memberRepository.existsByNickname(newNickname))
+        if (isNicknameTaken(member, nickname)) {
             throw new IllegalStateException("이미 사용 중인 닉네임입니다.");
-
-        member.setNickname(newNickname);
-    }
-
-    // 한 줄 소개 수정
-    private void updateIntroduction(Member member, String newIntroduction) {
-        if (newIntroduction != null) {
-            member.setIntroduction(newIntroduction);
         }
+        member.updateProfile(nickname, introduction, image);
     }
 
     // 회원 탈퇴
-    public void delete(Long id) {
-        Member member = memberRepository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 회원입니다."));
+    public void delete(Member member) {
         memberRepository.delete(member);
     }
 
