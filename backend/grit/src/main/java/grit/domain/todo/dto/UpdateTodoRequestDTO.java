a86ece8db00d6ac593b5288745dca5faf650c5bf
윰 <132874504+ymin431@users.noreply.github.com>
2026-03-09 16:38:13 +0900

    feat: 투두 그룹 연결 (#83)

--- backend/grit/src/main/java/grit/domain/todo/dto/UpdateTodoRequestDTO.java
+++ backend/grit/src/main/java/grit/domain/todo/dto/UpdateTodoRequestDTO.java
@@ -1,6 +1,6 @@
-package grit.todolist.dto;
+package grit.domain.todo.dto;
 
-import grit.todolist.Todo;
+import grit.domain.todo.Todo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,16 +9,17 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
-public class CreateTodoRequestDTO {
+public class UpdateTodoRequestDTO {
     @Schema(description = "투두 내용", example = "과제 제출하기")
     private String content;
+
+    @Schema(description = "완료 여부", example = "true")
+    private Boolean isDone;
 
     @Schema(description = "과목 카테고리", example = "SCHOOL")
     private Todo.SubjectCategory subjectCategory;
 
     @Schema(description = "마감일", example = "2025-01-25")
     private LocalDate dueDate;
-
-    @Schema(description = "방 ID (선택사항)", example = "1")
-    private Long roomId;
 }
+
--- backend/grit/src/main/java/grit/room/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package grit.room;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
-
