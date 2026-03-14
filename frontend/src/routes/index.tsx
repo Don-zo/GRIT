c3bd4c4b86efe0afead933eb582123379e2cf114
chahyunlee <chahyun023@gmail.com>
2026-03-14 22:32:11 +0900

    feat: Motion Framer를 사용하여 회원가입 페이지 애니메이션 구현

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "@/pages/Home";
 import LandingPage from "@/pages/Landing";
-import LoginPage from "@/pages/Login";
 import SignupPage from "@/pages/Signup";
 import RoomPage from "@/pages/Room";
 import GoogleOAuthRedirectPage from "@/pages/OAuthRedirect";
@@ -15,10 +14,6 @@ export const router = createBrowserRouter([
   {
     path: PATHS.LANDING,
     element: <LandingPage />,
-  },
-  {
-    path: PATHS.LOGIN,
-    element: <LoginPage />,
   },
   {
     path: PATHS.SIGNUP,
