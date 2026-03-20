7feab7f857aaf10e651ddfe72bad3926acf6570b
Junyoung Yang <y@jun0.dev>
2026-03-20 21:26:49 +0900

    refactor: 초대 코드 생성 시 사용되는 문자 집합 개선 (특수문자 제거)

--- backend/grit/src/main/java/grit/global/util/InviteCodeGenerator.java
+++ backend/grit/src/main/java/grit/global/util/InviteCodeGenerator.java
@@ -5,7 +5,7 @@ import java.security.SecureRandom;
 public class InviteCodeGenerator {
 
     // 가독성을 위해 0, O, 1, I 제외함
-    private static final String CHARACTERS = "23456789ABCDEFGHJKLMNPQRSTUVWXYZ!@*";
+    private static final String CHARACTERS = "23456789ABCDEFGHJKLMNPQRSTUVWXYZ";
     private static final int CODE_LENGTH = 6;
     private static final SecureRandom random = new SecureRandom();
 
