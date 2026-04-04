990ee40ffe40b4eb3ecf948477c6f54b8f251e78
chahyunlee <chahyun023@gmail.com>
2026-04-04 18:19:52 +0900

    refacotor: 툴팁 컴포넌트 분리

--- frontend/src/pages/Home/components/LeftSidebar.tsx
+++ frontend/src/pages/Home/components/LeftSidebar.tsx
@@ -5,6 +5,7 @@ import AddFriendModal from "@/pages/Home/components/Modals/AddFriendModal";
 import { QUERY_KEYS } from "@/apis/constants/queryKeys";
 import { friendApi } from "@/apis/services/friend";
 import type { FriendDetail } from "@/apis/types/friend";
+import FriendTooltip from "./FriendTooltip";
 
 type LeftSidebarProps = {
   onAddFriend?: () => void;
@@ -95,15 +96,7 @@ export default function LeftSidebar({
                 </button>
 
                 {hovered?.nickname === f.nickname && (
-                  <div
-                    role="tooltip"
-                    className="whitespace-nowrap absolute left-full top-1/2 z-50 ml-4 -translate-y-1/2 rounded-xl border border-white/30  bg-[#1e2228] px-3 py-2 text-center"
-                  >
-                    <div className="absolute left-0 top-1/2 h-2.5 w-2.5 -translate-x-1/2 -translate-y-1/2 rotate-45 border-l border-b border-white/30 bg-[#1e2228] " />
-                    <p className="text-sm font-semibold text-[#D6FDE5]">
-                      {f.nickname}
-                    </p>
-                  </div>
+                  <FriendTooltip nickname={f.nickname} />
                 )}
               </div>
             );
